35878b5436b8168d3e520e7b59512a496113b755
Kanishka C <kcwilliam35@gmail.com>
2020-07-22 19:18:56 +0530

    Initial Commit

---
+++ quote-generator/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  quote-generator
+//
+//  Created by Kanishka on 22/07/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ quote-generator/quote_generatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  quote_generatorApp.swift
+//  quote-generator
+//
+//  Created by Kanishka on 22/07/20.
+//
+
+import SwiftUI
+
+@main
+struct quote_generatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
